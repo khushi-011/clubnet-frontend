0ab8692b0abd784a4db0360f78980052c11a995a
Khushi Sen <khushisen6264@gmail.com>
2026-04-29 16:31:06 +0530

    fix vite allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),
   ],
+  server: {
+    host: true,
+    allowedHosts: ["mineduck.in", "www.mineduck.in"]
+  }
 })
